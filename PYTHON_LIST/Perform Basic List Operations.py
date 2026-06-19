eea03c060fe8515ee1f4ce6723bb2dbd7173d55b
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-06-19 23:22:45 +0530

    Add script for basic list operations in Python

---
+++ PYTHON_LIST/Perform Basic List Operations.py
@@ -0,0 +1,11 @@
+'''Practice Problem: Write a script to perform the following three operations on given list
+
+Access the third element of a list
+List Length: Print the total number of items
+Check if the list is empty'''
+
+Number = [10,20,30,40,50]
+print(Number[2])
+print(len(Number))
+a = len(Number) == 0
+print(a)
