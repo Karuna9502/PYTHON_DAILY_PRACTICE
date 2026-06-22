43d33b1edbe2846787e6b0a0caa91f40e3440589
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-06-22 09:12:28 +0530

    Create script to find max and min in a list
    Add a script to find maximum and minimum values in a list.

---
+++ Find Maximum and Minimum from List.py
@@ -0,0 +1,11 @@
+'''Practice Problem: Identify the largest and smallest numerical values within a provided list.
+
+Exercise Purpose: Finding extremes is vital for tasks like identifying the “best” price,
+ the “highest” score, or detecting “outlier” data points in a dataset.'''
+
+input = [45, 12, 89, 2, 67]
+Maximum = max(input)
+Minimum = min(input)
+
+print("Maximum = ", Maximum)
+print("Minimum = ", Minimum)
