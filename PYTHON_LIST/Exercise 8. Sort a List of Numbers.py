99c6943478d1018dc1270874056fbb63f00be814
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-06-26 22:47:18 +0530

    Enhance sorting script with method explanation
    Added explanation about the sort method and its complexity.

---
+++ PYTHON_LIST/Exercise 8. Sort a List of Numbers.py
@@ -0,0 +1,9 @@
+# Practice Problem: Sort a list of numbers in ascending order (lowest to highest).
+
+input = [56, 12, 89, 3, 22 ]
+input.sort()
+
+print(f"sorted List: {input}")
+
+'''.sort(): This performs an “in-place” sort. It doesn’t return anything; it simply rearranges the elements at their current memory addresses.
+Complexity: Python’s sorting is O(n log n), which is the gold standard for comparison-based sorting efficiency'''
