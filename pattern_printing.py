7073ef2501c9e98627dce80e213a44f09b2954b4
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-07-22 23:24:16 +0530

    Add pattern printing functionality in Python

---
+++ pattern_printing.py
@@ -0,0 +1,32 @@
+'''****
+   ****
+   ****
+   ****'''
+
+for i in range(4):
+    for j in range(4):
+        print("*",end='')
+    print()
+
+'''*
+   **
+   ***
+   ****
+   *****'''
+
+
+for i in range(5):
+    for j in range(i+1):
+        print("*",end='')
+    print()
+
+'''1
+   12
+   123
+   1234
+   12345'''
+
+for i in range(6):
+    for j in range(1,i+1):
+        print(i,end='')
+    print()
