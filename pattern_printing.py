0a52a3d48ce97879a1bc977aaec69cf7f3ae5151
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-07-26 21:47:27 +0530

    Fix pattern printing logic in pattern_printing.py

--- pattern_printing.py
+++ pattern_printing.py
@@ -41,3 +41,14 @@ for i in range(6,0,-1):
     for j in range(i-1):
         print("*",end='')
     print()
+
+'''12345
+   1234
+   123
+   12
+   1'''
+
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
