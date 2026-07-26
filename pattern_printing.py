99af26aacf2b1091a8ffb091114979c230d91d09
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-07-26 21:46:53 +0530

    Add pattern printing for decreasing star pattern

--- pattern_printing.py
+++ pattern_printing.py
@@ -30,3 +30,14 @@ for i in range(6):
     for j in range(1,i+1):
         print(i,end='')
     print()
+
+'''*****
+   ****
+   ***
+   **
+   *'''
+
+for i in range(6,0,-1):
+    for j in range(i-1):
+        print("*",end='')
+    print()
