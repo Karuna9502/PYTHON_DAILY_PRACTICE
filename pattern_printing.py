e6fa9f3cc5f6e93aa5e9fe19347a9311100a0974
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-07-29 12:55:53 +0530

    Add pattern printing for stars in pyramid shape

--- pattern_printing.py
+++ pattern_printing.py
@@ -103,3 +103,22 @@ for i in range(5,0,-1):
     for j in range(2 * i - 1):
         print("*",end='')
     print()
+
+'''*
+   **
+   ***
+   ****
+   *****
+   ****
+   ***
+   **
+   *'''
+
+for i in range(5):
+    for j in range(i+1):
+        print("*",end='')
+    print()
+for i in range(5, 1, -1):
+    for j in range(i-1):
+        print("*",end='')
+    print()
