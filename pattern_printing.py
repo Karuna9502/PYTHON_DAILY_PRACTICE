501a5cc3983f7ac808748b5f1d861ea493235e3d
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-07-29 12:55:34 +0530

    Implement diamond pattern printing in pattern_printing.py
    Added code to print a diamond pattern using stars.

--- pattern_printing.py
+++ pattern_printing.py
@@ -80,3 +80,26 @@ for i in range(5, 0, -1):              # Loops down from 5 to 1
         print("*", end='')
     print()                            # Moves to the next line
 
+'''  *
+    ***
+   *****
+  *******
+ *********
+ *********
+  *******
+   *****
+    ***
+     *'''
+
+for i in range(5):
+    for j in range(5-i):
+        print(' ',end='')
+    for j in range(2 * i +1):
+        print("*",end='')
+    print()
+for i in range(5,0,-1):
+    for j in range(6-i):
+        print(' ',end='')
+    for j in range(2 * i - 1):
+        print("*",end='')
+    print()
