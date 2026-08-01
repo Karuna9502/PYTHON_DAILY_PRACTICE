19348a88bdf7d1c30e3baed80b37ecdac65c3ccf
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-08-01 12:32:11 +0530

    Implement pattern printing of binary sequences
    Add pattern printing for alternating binary sequences.

--- pattern_printing.py
+++ pattern_printing.py
@@ -122,3 +122,20 @@ for i in range(5, 1, -1):
     for j in range(i-1):
         print("*",end='')
     print()
+
+'''1
+   10
+   101
+   1010
+   10101'''
+
+
+for i in range(5):
+    if i % 2 == 0:
+        start = 1
+    else:
+        start = 0
+    for j in range(i+1):
+        print(start,end='')
+        start = 1 - start
+    print()
