5d68dd3a1c28758633a63decc3618f8533348168
KARUNA KUMARI <144718053+Karuna9502@users.noreply.github.com>
2026-08-11 22:25:46 +0530

    Implement lower half of pattern printing
    Add lower half pattern printing to the function.

--- pattern_printing.py
+++ pattern_printing.py
@@ -280,5 +280,22 @@ for i in range(n, 0, -1):
     print()
 
     spaces += 2
+  # Lower Half
+spaces = 2 * (n - 1)
+
+for i in range(1, n + 1):
+
+    for j in range(i):
+        print("*", end="")
+
+    for j in range(spaces):
+        print(" ", end="")
+
+    for j in range(i):
+        print("*", end="")
+
+    print()
+
+    spaces -= 2
 
 
